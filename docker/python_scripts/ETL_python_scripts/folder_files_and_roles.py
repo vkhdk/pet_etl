91fc8e7689f7db749c8c96f7adf0e0b7af616fa3
Vova <vkhdk@yandex.ru>
2023-01-27 14:31:24 +0300

    update folder structure

--- docker/python_scripts/ETL_python_scripts/folder_files_and_roles.py
+++ docker/python_scripts/ETL_python_scripts/folder_files_and_roles.py
@@ -1,14 +1,14 @@
 #here are all the prod files from the project with a description and roles
 
 #here are all the links and authorization data
-ETL_secrets = 'ETL_secrets.py'
+ETL_secrets = './ETL_secrets.py'
 
 #everything related to connecting to the API or other data source
-get_data_from_api = 'get_data_from_api.py'
+get_data_from_api = './get_data_from_api.py'
 
 #transformation utility
 ##converting a BeautifulSoup object to JSON
-get_weather_from_google_search = 'get_weather_from_google_search.py'
+get_weather_from_google_search = './get_weather_from_google_search.py'
 
 #storage of the last error log
 errors_log = './errors_log.txt'
@@ -17,10 +17,10 @@ errors_log = './errors_log.txt'
 content = './content.txt'
 
 #debug script
-manual_run = 'manual_run.py'
+manual_run = './manual_run.py'
 
 #manual_run log
 manual_run_log = './manual_run.log'
 
 #universal functions
-ETL_utilities = 'ETL_utilities.py'
+ETL_utilities = './ETL_utilities.py'
